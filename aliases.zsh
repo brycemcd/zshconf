7e2afc32d4b8f3b2741fffde62e8652bf8b8f767
Bryce McDonnell <bryce.mcdonnell@verbalizeit.com>
2014-06-29 18:22:08 -0400

    new aliases and bindkeys

--- aliases.zsh
+++ aliases.zsh
@@ -4,6 +4,7 @@ alias shipit="git push origin master && git push heroku master"
 alias so="source ~/.zshrc"
 alias gcb="git branch | xargs -n 1 git branch -d"
 alias osock="netstat -nta | grep LISTEN"
+alias zc="zcalc"
 
 alias -g G="| grep "
 alias -g NUL="> /dev/null 2>&1"
--- bindkeys.zsh
+++ bindkeys.zsh
@@ -1,2 +1,4 @@
 # can't get used to any other search in the shell
 bindkey '^R' history-incremental-search-backward
+
+autoload -U zcalc
