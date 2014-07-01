e60b96a042e3c8ad84b54eaa9e24f6cb25cd01e1
Bryce McDonnell <bryce@cricketscircle.com>
2014-07-01 15:43:05 -0400

    adds a couple of hashes

--- aliases.zsh
+++ aliases.zsh
@@ -4,10 +4,16 @@ alias shipit="git push origin master && git push heroku master"
 alias so="source ~/.zshrc"
 alias gcb="git branch | xargs -n 1 git branch -d"
 alias osock="netstat -nta | grep LISTEN"
+
 alias zc="zcalc"
+
+## RAILS STUFF
+alias rs="rails server"
 
 alias -g G="| grep "
 alias -g NUL="> /dev/null 2>&1"
 
 hash -d cweb=$HOME/Cricket/web
 hash -d cblog=$HOME/Cricket/blog_theme
+hash -d oldz=$HOME/.oh-my-zsh
+hash -d zshrc=$HOME/.zshconf
