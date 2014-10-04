34a8119f0285786dfd97fe67ad268eeedc879dfd
Bryce McDonnell <bryce@cricketscircle.com>
2014-10-04 11:07:07 -0400

    adds a couple of aliases

--- aliases.zsh
+++ aliases.zsh
@@ -7,6 +7,10 @@ alias osock="netstat -nta | grep LISTEN"
 
 alias zc="zcalc"
 
+alias openusb="encfs /media/brycemcd/USB20FD/encrypted /media/brycemcd/USB20FD/visible"
+alias closeusb="fusermount -u /media/brycemcd/USB20FD/visible"
+alias dockstudio="sudo docker run -d -p 22 -p 8787 --name rstudio -v /media/brycemcd/filestore/third_party_data/:/opt/data -v /home/brycemcd/Stats/cricket_stats/:/opt/cricket_stats angelrr7702/rstudio"
+
 ## RAILS STUFF
 alias rs="rails server"
 
@@ -18,3 +22,4 @@ hash -d cblog=$HOME/Cricket/blog_theme
 hash -d sites=$HOME/Sites
 hash -d oldz=$HOME/.oh-my-zsh
 hash -d zshrc=$HOME/.zshconf
+hash -d data=/media/brycemcd/filestore/third_party_data
