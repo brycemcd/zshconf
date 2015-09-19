e6c5def254dd67696e545a0aa5adb9db2103fb6e
Bryce McDonnell <bryce@cricketscircle.com>
2015-09-19 13:58:11 -0400

    updates a couple of aliases for scala and sbt

--- aliases.zsh
+++ aliases.zsh
@@ -11,6 +11,9 @@ alias openusb="encfs /media/brycemcd/USB20FD/encrypted /media/brycemcd/USB20FD/v
 alias closeusb="fusermount -u /media/brycemcd/USB20FD/visible"
 alias dockstudio="sudo docker run -d -p 22 -p 8787 -v /media/brycemcd/filestore/third_party_data/:/home/rstudio/data -v /home/brycemcd/Stats/cricket_stats/:/home/rstudio/cricket_stats rstudio"
 
+alias sbtboot="echo 'target/' > .gitignore"
+alias buildsbt="mkdir -p src/main/resources && mkdir src/main/scala && mkdir src/main/java && mkdir -p src/test/resources && mkdir src/test/scala && mkdir -p src/test/java && touch build.sbt"
+
 ## RAILS STUFF
 alias rs="rails server"
 
@@ -20,6 +23,8 @@ alias -g webs="python -m SimpleHTTPServer 8000"
 alias -g G="| grep "
 alias -g NUL="> /dev/null 2>&1"
 
+alias -g P='| jq -c ".hits.hits[] | {score: ._score, fname: ._source.first_name, lname: ._source.last_name}" | less'
+
 hash -d cweb=$HOME/Cricket/web
 hash -d cblog=$HOME/Cricket/blog_theme
 hash -d sites=$HOME/Sites
@@ -28,3 +33,4 @@ hash -d zshrc=$HOME/.zshconf
 hash -d todo=$HOME/Sites/todo
 hash -d data=/media/brycemcd/filestore/third_party_data
 hash -d usbenc=/media/brycemcd/USB20FD/visible
+hash -d etl=$HOME/Cricket/cricket-etl
